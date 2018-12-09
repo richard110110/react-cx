d591c79f2149898dcb471a602e2b96fe5d6fd4de
u6695264 <u6695264@anu.edu.au>
2018-12-10 04:11:49 +1100

    finish event created

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 // import { Button } from 'semantic-ui-react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
+import {Container} from 'semantic-ui-react';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <NavBar/>
+        <Container className = "main">
        <EventDashboard/>
+       </Container>
       </div>
     );
   }
